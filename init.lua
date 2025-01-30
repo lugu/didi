61bb6742aa072585778619a719545bd7bb71f8c2
Ludovic Guegan <ludovic.guegan@gmail.com>
2025-01-30 22:57:57 +0000

    Day 1: make a custom cube

---
+++ init.lua
@@ -0,0 +1,23 @@
+print("Welcome to didi - from the mod")
+minetest.register_node("didi:crytal1", {
+	description = "Le crytal d'Orion",
+	tiles = {
+		"crytal_block_1.png",
+		"crytal_block_1.png",
+		"crytal_block_1.png",
+		"crytal_block_1.png",
+		"crytal_block_1.png",
+		"crytal_block_1.png",
+	},
+})
+minetest.register_node("didi:cannon1", {
+	description = "Le cannon d'Yvain",
+	tiles = {
+		"cannon_block_1.png",
+		"cannon_block_1.png",
+		"cannon_block_1.png",
+		"cannon_block_1.png",
+		"cannon_block_1.png",
+		"cannon_block_1.png",
+	},
+})
